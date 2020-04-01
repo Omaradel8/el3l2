a77fc0535a9716b6af0549de21dc476cda0017b8
Omar Adel <omar88adel88@gmail.com>
2020-04-01 19:50:01 +0200

    fuckfuckfuckfuck

--- el3l2/ViewController.swift
+++ el3l2/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let a = 2
 
+    @IBOutlet weak var ve: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let vc = storyboard?.instantiateViewController(withIdentifier: "view3")
+        let viewvc = vc?.view
+        viewvc?.frame.size.height = ve.frame.height
+        viewvc?.frame.size.width = ve.frame.width
+        
+        let vc2 = storyboard?.instantiateViewController(withIdentifier: "view5")
+        let viewvc2 = vc?.view
+        viewvc2?.frame.size.height = ve.frame.height
+        viewvc2?.frame.size.width = ve.frame.width
+        
+        if a == 1{
+            ve.addSubview(vc!.view)
+        }else{
+            ve.addSubview(vc2!.view)
+        }
     }
 
 
